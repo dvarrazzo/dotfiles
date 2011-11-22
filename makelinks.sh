1ad498abd4e7fe4085e20257491edd1670285085
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2011-11-22 11:38:47 +0000

    Added script to create links to config files on a new system

---
+++ makelinks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Create all the links to config files I may want on a new system
+
+ln -s etc/bash_aliases .bash_aliases
+ln -s etc/ackrc .ackrc
+ln -s etc/git/gitconfig .gitconfig
+ln -s etc/git/gitignore .gitignore
+ln -s etc/hg/hgrc .hgrc
+ln -s etc/less/lessfilter .lessfilter
+ln -s etc/tmux.conf .tmux.conf
+ln -s etc/vim .vim
+ln -s etc/vim/vimrc .vimrc
