b9064370072b10e02bedb1adbc361f4352ae1734
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2011-12-20 17:01:01 +0000

    awesome -- change tag with Win + Page Up/Down

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -174,6 +174,8 @@ root.buttons(awful.util.table.join(
 globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
+    awful.key({ modkey,           }, "Page_Up",   awful.tag.viewprev       ),
+    awful.key({ modkey,           }, "Page_Down",  awful.tag.viewnext       ),
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
 
     awful.key({ modkey,           }, "j",
