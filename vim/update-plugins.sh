f61655a94b5f75918d8a51379b076581628ab13b
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2020-02-03 03:18:44 +0000

    vim: added script to update plugins

---
+++ vim/update-plugins.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+# set -x
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function log() {
+    echo $1 >&2
+}
+
+for d in $(cd ${dir}/.. && ls -d vim/pack/*/*/*/); do
+    # Update only if it's a submodule
+    # ${d%/} strips trailing slash
+    if ! grep -q ${d%/} "${dir}/../.gitmodules"; then
+        continue
+    fi
+    log "updating package $(basename $d)"
+    git -C "../$d" checkout master
+    git -C "../$d" pull
+done
