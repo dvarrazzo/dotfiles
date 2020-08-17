a9a83839ee4bcfc53e3c9411908aaa32e0f9345a
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2020-08-17 13:15:00 +0100

    makelinks: idempotent (don't create nested links for subdirs)

--- makelinks.sh
+++ makelinks.sh
@@ -2,28 +2,55 @@
 #
 # Create all the links to config files I may want on a new system
 
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$dir/.."
+set -euo pipefail
+# set -x
 
-ln -s $dir/bash_aliases .bash_aliases
-ln -s $dir/bash_completion .bash_completion
-ln -s $dir/ackrc .ackrc
-ln -s $dir/git/gitconfig .gitconfig
-ln -s $dir/git/gitignore .gitignore
-ln -s $dir/git/git_template .git_template
-ln -s $dir/hg/hgrc .hgrc
-ln -s $dir/less/lessfilter .lessfilter
-ln -s $dir/psqlrc .psqlrc
-ln -s $dir/sqliterc .sqliterc
-ln -s $dir/tmux.conf .tmux.conf
-ln -s $dir/vim .vim
-ln -s $dir/vim/vimrc .vimrc
-ln -s $dir/gdbinit .gdbinit
-mkdir -p .darcs
-ln -s $dir/darcs/defaults .darcs/
-ln -s $dir/xsession/xsession .xsession
-mkdir -p .config
-ln -s $dir/flake8 .config/
-mkdir -p .config/i3
-ln -s $dir/i3/config .config/i3/
-ln -s $dir/i3/i3blocks/ .config/i3blocks
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+link () {
+    what="$dir"/"$1"
+    where=$2
+
+    if [ ! -e "$what" ]; then
+        echo "source not found: '$what'" >&2
+        exit 1
+    fi
+
+    if [ -L "$where" ] && [ ! -e "$where" ]; then
+        echo "broken link: '$where'" >&2
+        exit 1
+    fi
+
+    if [ -e "$where" ]; then
+        echo "file exists: '$where'" >&2
+        return
+    fi
+
+    d=$(dirname "$where")
+    if [ ! -d "$d" ]; then
+        mkdir -vp "$d"
+    fi
+
+    ln -sv "$what" "$where"
+}
+
+
+link bash_aliases ~/.bash_aliases
+link bash_completion ~/.bash_completion
+link ackrc ~/.ackrc
+link git/gitconfig ~/.gitconfig
+link git/gitignore ~/.gitignore
+link git/git_template ~/.git_template
+link hg/hgrc ~/.hgrc
+link less/lessfilter ~/.lessfilter
+link psqlrc ~/.psqlrc
+link sqliterc ~/.sqliterc
+link tmux.conf ~/.tmux.conf
+link vim ~/.vim
+link vim/vimrc ~/.vimrc
+link gdbinit ~/.gdbinit
+link darcs/defaults ~/.darcs/defaults
+link xsession/xsession ~/.xsession
+link flake8 ~/.config/flake8
+link i3/config ~/.config/i3/config
+link i3/i3blocks/ ~/.config/i3blocks
