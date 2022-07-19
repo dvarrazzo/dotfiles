a33fe2d1d6df61241a8700f1e0acd9d43974a848
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2022-07-19 11:17:52 +0100

    baloo: vertical display on the right

--- bin/display-benq-vertical.sh
+++ bin/display-benq-vertical.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 2160x1670 --rotate normal \
-    --output DP-1-1 --mode 3840x2160 --pos 0x0 --rotate left --output DP-1-2 --off \
+
+# Hint: use arandr; see ~/.screenlay
+
+# On the left
+# xrandr --output eDP-1 --primary --mode 1920x1080 --pos 2160x1670 --rotate normal \
+#     --output DP-1-1 --mode 3840x2160 --pos 0x0 --rotate left --output DP-1-2 --off \
+#     --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-1-3 --off
+
+# On the right
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x2000 --rotate normal \
+    --output DP-1-1 --mode 3840x2160 --pos 1920x0 --rotate right --output DP-1-2 --off \
     --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-1-3 --off
